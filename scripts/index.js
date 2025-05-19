28e34bc0cd1efb78506d5fcb63ee60c8bb361638
Maria Glazkova <marusya86_msk@yandex.ru>
2025-05-20 00:47:48 +0300

    first version

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,40 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const content = document.querySelector(".content");
+const places = content.querySelector(".places");
+const placesList = places.querySelector(".places__list");
 
-// @todo: DOM узлы
+function createCard(card, deleteCard) {
+  const cardElement = cardTemplate.cloneNode(true);
 
-// @todo: Функция создания карточки
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardTitle.textContent = card.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener('click', function() {
+    const cardItem = deleteButton.closest('.card');
+    deleteCard(cardItem);
+  });
+
+  return cardElement;
+}
+
+function deleteCard(card) {
+  card.remove();
+}
+
+initialCards.forEach(function(card){
+  const cardItem = createCard(card, deleteCard);
+  placesList.append(cardItem);
+})
+
+
+
+
+
 
 // @todo: Функция удаления карточки
 
